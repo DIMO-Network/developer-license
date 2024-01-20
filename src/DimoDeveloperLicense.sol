a7a978ffaf3a466d777d16211dac568db6a29ecc
Sean Matthew <s.matthew.english@gmail.com>
2024-01-20 16:57:31 -0500

    update

--- src/DimoDeveloperLicense.sol
+++ src/DimoDeveloperLicense.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Ownable2Step} from "@openzeppelin/contracts/access/Ownable2Step.sol";
 import {ERC721} from "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 import {DimoDeveloperLicenseAccount} from "./DimoDeveloperLicenseAccount.sol";
 
@@ -8,17 +10,23 @@ interface IDimoToken {
     function transferFrom(address from, address to, uint256 value) external returns (bool);
 }
 
-contract DimoDeveloperLicense is ERC721 {
-    uint256 public licenseCost;
+contract DimoDeveloperLicense is ERC721, Ownable2Step {
+
+    modifier onlyTokenOwner(uint256 tokenId) {
+        require(msg.sender == ownerOf(tokenId), "DimoDeveloperLicense: invalid msg.sender");
+        _;
+    }
+
+    uint256 public _licenseCost;
     uint256 private counter;
     IDimoToken private dimoToken;
 
+    event UpdateLicenseCost(uint256 licenseCost);
     event LicenseMinted(uint256 indexed tokenId, address indexed owner, address indexed account, string clientId);
     event RedirectUriEnabled(uint256 indexed tokenId, string uri);
     event SignerEnabled(uint256 indexed tokenId, address indexed signer);
 
     error ClientIdTaken();
-    error Unauthorized();
 
     mapping(uint256 => address) private accounts;
     mapping(uint256 => mapping(string => bool)) private redirectUris;
@@ -27,9 +35,9 @@ contract DimoDeveloperLicense is ERC721 {
     mapping(uint256 => string) tokenIdToClientId;
     mapping(string => uint256) clientIdToTokenId;
 
-    constructor(address dimoTokenAddress, uint256 licenseCost_) ERC721("DIMO Developer License", "DDL") {
+    constructor(address dimoTokenAddress, uint256 licenseCost_) ERC721("DIMO Developer License", "DDL") Ownable(msg.sender) {
         dimoToken = IDimoToken(dimoTokenAddress);
-        licenseCost = licenseCost_;
+        _licenseCost = licenseCost_;
     }
 
     function mint(string calldata clientId) public returns (uint256, address) {
@@ -37,7 +45,7 @@ contract DimoDeveloperLicense is ERC721 {
             revert ClientIdTaken();
         }
 
-        dimoToken.transferFrom(msg.sender, address(this), licenseCost);
+        dimoToken.transferFrom(msg.sender, address(this), _licenseCost);
 
         uint256 tokenId = ++counter;
         tokenIdToClientId[tokenId] = clientId;
@@ -52,26 +60,33 @@ contract DimoDeveloperLicense is ERC721 {
         return (tokenId, accountAddress);
     }
 
-    function enableRedirectUri(uint256 tokenId, string calldata uri) public {
-        if (msg.sender != ownerOf(tokenId)) {
-            revert Unauthorized();
-        }
-
+    function enableRedirectUri(uint256 tokenId, string calldata uri) public onlyTokenOwner(tokenId) {
         redirectUris[tokenId][uri] = true;
-
         emit RedirectUriEnabled(tokenId, uri);
     }
 
-    function enableSigner(uint256 tokenId, address signer) public {
-        if (msg.sender != ownerOf(tokenId)) {
-            revert Unauthorized();
-        }
-
+    function enableSigner(uint256 tokenId, address signer) public onlyTokenOwner(tokenId) {
         signers[tokenId][signer] = true;
         emit SignerEnabled(tokenId, signer);
     }
 
     function isSigner(uint256 tokenId, address signer) public view returns (bool) {
         return signers[tokenId][signer];
+    }
+
+    /**
+     * @dev Sets a new cost for the DIMO developer license.
+     * Can only be called by the contract owner.
+     *
+     * Emits an {UpdateLicenseCost} event indicating the updated license cost.
+     *
+     * Requirements:
+     * - The caller must be the contract owner.
+     *
+     * @param licenseCost_ The new cost of the license in DIMO tokens.
+     */
+    function setLicenseCost(uint256 licenseCost_) public onlyOwner {
+        _licenseCost = licenseCost_;
+        emit UpdateLicenseCost(licenseCost_);
     }
 }
