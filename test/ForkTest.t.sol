258d6807072681a5cea5fb494d037b18ea181474
Sean Matthew <s.matthew.english@gmail.com>
2024-01-20 16:38:00 -0500

    update test

--- test/ForkTest.t.sol
+++ test/ForkTest.t.sol
@@ -2,20 +2,28 @@
 pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
+import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 //forge test --match-path ./test/ForkTest.t.sol -vv
 contract ForkTest is Test {
 
     function setUp() public {
+
+        //0xE261D618a959aFfFd53168Cd07D12E37B26761db
+        //^DIMO Token Polygon
         
         vm.createSelectFork('https://polygon-mainnet.g.alchemy.com/v2/NlPy1jSLyP-tUCHAuilxrsfaLcFaxSTm', 50573735);
-          
     }
 
     /**
      * 
      */
     function test_001() public {
+
+        deal(0xE261D618a959aFfFd53168Cd07D12E37B26761db, address(this), 1 ether);
+
+        uint256 balance = ERC20(0xE261D618a959aFfFd53168Cd07D12E37B26761db).balanceOf(address(this));
+        assertEq(balance, 1 ether);
     
     }
 
--- test/ProxyTest.t.sol
+++ test/ProxyTest.t.sol
@@ -5,25 +5,20 @@ import {Test, console2} from "forge-std/Test.sol";
 
 import {Proxy} from "../src/test/Proxy.sol";
 import {Dimo} from "../src/test/DimoV2.sol";
+import {DimoChildTokenV1} from "../src/test/DimoChildTokenV1.sol";
 
 //forge test --match-path ./test/ProxyTest.t.sol -vv
 contract ProxyTest is Test {
 
-    DimoV1 public dct1;
-    DimoV1 public proxiedDct1;
-    DimoV2 public dimo;
-    DimoV2 public proxiedDimo;
-
-    TwapV3 public twapV3;
-    DimoDataCredit public dimoDataCredit;
-    NormalizedPriceProvider public provider;
-    DimoMarketRewards public marketRewards;
+    DimoChildTokenV1 public dct1;
+    DimoChildTokenV1 public proxiedDct1;
+    Dimo public dimo;
+    Dimo public proxiedDimo;
 
     address public user;
     uint256 public amount;
 
     function setUp() public {
-        vm.createSelectFork('https://polygon-mainnet.g.alchemy.com/v2/NlPy1jSLyP-tUCHAuilxrsfaLcFaxSTm', 50573735);
 
         user = address(0x1337);
         amount = 1 ether;   
@@ -46,67 +41,26 @@ contract ProxyTest is Test {
         bytes32 defaultAdminRole = 0x00;
         address account = address(this);
         bool hasRole = proxiedDct1.hasRole(defaultAdminRole, account);
-        assertEq(hasRole, true);
 
         // (5) Upgrade to a new logic contract
         proxy.upgradeTo(address(dimo));
         proxiedDimo = Dimo(address(proxiedDct1));
 
-        // (6) Now calling this function should actually works
+        // (6) Now calling this function should actually work
         proxiedDimo.grantRole(keccak256("MINTER_ROLE"), address(this));
     }
 
     /**
-     *   [17972] ForkTest::test_001() 
-     *   ├─ [0] console::log(***) [staticcall]
-     *   │   └─ ← ()
-     *   ├─ [9082] 0x41e64a5Bc929fA8E6a9C8d7E3b81A13b21Ff3045::observe([300, 0]) [staticcall]
-     *   │   └─ ← "OLD"
-     *   └─ ← "OLD"
      *   
      */
     function test_001() public {
 
-        address to = user;
-        uint256 amountIn = amount;
-        bytes memory data = "";
-        proxiedDimo.mint(to, amountIn);
+        proxiedDimo.mint(user, amount);
 
-        uint256 balance00 = proxiedDimo.balanceOf(to);
-        assertEq(balance00, amountIn);
+        uint256 balance00 = proxiedDimo.balanceOf(user);
+        assertEq(balance00, amount);
 
-        uint32 twapIntervalUsdc = 1 minutes;
-        console2.log("twapIntervalUsdc: %s", twapIntervalUsdc);
-        uint32 twapIntervalDimo = 1 minutes; 
-        console2.log("twapIntervalDimo: %s", twapIntervalDimo);
-
-        twapV3 = new TwapV3();
-        twapV3.initialize(twapIntervalUsdc, twapIntervalDimo);
-        marketRewards = new DimoMarketRewards();
-        provider = new NormalizedPriceProvider();
-        provider.addOracleSource(address(twapV3));
-        dimoDataCredit = new DimoDataCredit();
-        dimoDataCredit.initialize(
-            address(proxiedDimo), 
-            address(0x123), 
-            address(provider), 
-            1 days
-        );
-
-        vm.startPrank(to);
-        proxiedDimo.approve(address(dimoDataCredit), amountIn);
-        vm.stopPrank();
-
-        uint256 allowance = proxiedDimo.allowance(to, address(dimoDataCredit));
-        assertEq(allowance, amountIn);
-
-        dimoDataCredit.mint(to, amountIn, data);
-
-        uint256 balance01 = proxiedDimo.balanceOf(to);
-        assertEq(balance01, 0);
-
-        uint256 balance02 = dimoDataCredit.balanceOf(to);
-        console2.log("balance02: %s", balance02);
+        
     }
 
 }
