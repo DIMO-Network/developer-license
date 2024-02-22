78cd2f9eb64d7a6c658e9e9738520affae89a348
Sean Matthew <s.matthew.english@gmail.com>
2024-02-22 16:22:32 -0500

    update

--- test/Integration.t.sol
+++ test/Integration.t.sol
@@ -5,15 +5,20 @@ import {console2} from "forge-std/Test.sol";
 
 import {BaseSetUp} from "./helper/BaseSetUp.t.sol";
 
+import {IERC1271} from "openzeppelin-contracts/contracts/interfaces/IERC1271.sol";
+
+import {IDimoDeveloperLicenseAccount} from "../src/interface/IDimoDeveloperLicenseAccount.sol";
+
 //forge test --match-path ./test/Integration.t.sol -vv
 contract IntegrationTest is BaseSetUp {
 
     uint256 tokenId;
+    address clientId;
 
     function setUp() public {
         _setUp();
 
-        (tokenId,) = license.issueInDimo();
+        (tokenId, clientId) = license.issueInDimo();
         assertEq(tokenId, 1);
         assertEq(license.ownerOf(tokenId), address(this));
     }
@@ -28,8 +33,25 @@ contract IntegrationTest is BaseSetUp {
         ///@notice check that the backend has that
 
         ///@dev add a signer, with a known pk
+        uint256 privateKey = 0x1337;
+        address signer = vm.addr(privateKey);
+        
+        license.enableSigner(tokenId, signer);
+
+        bytes32 hashValue = keccak256(
+            abi.encodePacked(
+                keccak256(
+                    "\x19Ethereum Signed Message:\n32"
+                ),
+                keccak256("challenge message")
+            )   
+        );
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(privateKey, hashValue);
+        bytes memory signature = abi.encodePacked(r, s, v); 
+        bytes4 output = IDimoDeveloperLicenseAccount(clientId).isValidSignature(hashValue, signature);
 
         ///@dev use that signer to sign a challenge message for that license
+        assertEq(IERC1271.isValidSignature.selector, output);
 
         ///@notice check that you can log in as the license address
 
