4ffd28b840cabbf4e307e739288e07face70f045
Sean Matthew <s.matthew.english@gmail.com>
2024-02-28 13:40:28 -0500

    update

--- src/test/TestToken.sol
+++ src/test/TestToken.sol
@@ -6,10 +6,10 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract TestToken is ERC20 {
     
     constructor() ERC20("TestToken", "TT") {
-        _mint(msg.sender, 10_000);
+        _mint(msg.sender, 1_000_000 ether);
     }
 
-    function mint(address account, uint256 value) internal {
+    function mint(address account, uint256 value) external {
         _mint(account, value);
     }
 }
