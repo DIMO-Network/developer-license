c41c95caaac68d00a09705be020e90e6f9e1d2a8
Sean Matthew <s.matthew.english@gmail.com>
2024-03-28 13:58:51 -0400

    doc update

--- src/LicenseAccountFactory.sol
+++ src/LicenseAccountFactory.sol
@@ -15,22 +15,40 @@ import {ILicenseAccountFactory} from "./interface/ILicenseAccountFactory.sol";
  * @custom:coauthor Yevgeny Khessin (@zer0stars)
  * @custom:coauthor Rob Solomon (@robmsolomon)
  * @custom:contributor Allyson English (@aesdfghjkl666)
+ * @notice Manages the creation of DIMO Developer License Account instances, enabling the deployment 
+ *         of minimal proxy contracts for each new license account. This factory pattern minimizes the 
+ *         gas cost for deploying many instances of license accounts by using EIP-1167 clone contracts.
+ * @dev Inherits from Ownable2Step for secure ownership transfer and implements ILicenseAccountFactory 
+ *      for creating license accounts.
  */
 contract LicenseAccountFactory is Ownable2Step, ILicenseAccountFactory { 
 
+    /// @notice Address of the license account template for cloning.
     address public _template;
+
+    /// @notice Address of the DIMO Developer License contract.
     address public _license; 
 
+    /**
+     * @dev Initializes the contract by creating a new DimoDeveloperLicenseAccount as a template 
+     *      for future clones and sets the owner of the contract.
+     */
     constructor() Ownable(msg.sender) {
         _template = address(new DimoDeveloperLicenseAccount());
     }
 
+    /**
+     * @notice Sets the address of the DIMO Developer License contract.
+     * @param license_ The address of the DIMO Developer License contract.
+     */
     function setLicense(address license_) external onlyOwner {
         _license = license_;
     }
 
     /**
-     * 
+     * @notice Creates a new clone of the DimoDeveloperLicenseAccount, initializing it with a token ID and the license contract address.
+     * @param tokenId The token ID to associate with the newly created license account.
+     * @return clone The address of the newly created clone account.
      */
     function create(uint256 tokenId) external returns (address clone) {
         clone = _createClone(address(_template));
