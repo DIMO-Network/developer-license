36131b215015b81995cbeddbd121fa9aa184e4fa
Lorran Sutter <sutterlorran@gmail.com>
2024-07-29 23:28:34 -0400

    update IDimoCredit

--- src/interface/IDimoCredit.sol
+++ src/interface/IDimoCredit.sol
@@ -1,13 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-interface IDimoCredit {
-    function receiver() external view returns (address);
+import {IAccessControl} from "@openzeppelin/contracts/access/IAccessControl.sol";
+
+interface IDimoCredit is IAccessControl {
     function dimoCreditRate() external view returns (uint256);
+    function periodValidity() external view returns (uint256);
+    function receiver() external view returns (address);
+    function dimoToken() external view returns (address);
+    function provider() external view returns (address);
+    function decimals() external pure returns (uint8);
+    function name() external view returns (string memory);
+    function symbol() external view returns (string memory);
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
     function burn(address from, uint256 amount) external;
-    function transferFrom(address from, address to, uint256 value) external returns (bool);
-    function mintAmountDc(address to, uint256 dimoCredits, bytes calldata data) external returns (uint256 amountIn);
+    function mintInDimo(address to, uint256 amountDimo) external returns (uint256 dimoCredits);
     function mintInDimo(address to, uint256 amountDimo, bytes calldata data) external returns (uint256 dimoCredits);
+    function mint(address to, uint256 amountDimoCredits) external;
     function mint(address to, uint256 amountDimoCredits, bytes calldata data) external;
-    function grantRole(bytes32 role, address account) external;
+    function updatePrice(bytes calldata data) external;
+    function setDimoCreditRate(uint256 dimoCreditRateInWei_) external;
+    function setDimoTokenAddress(address dimoTokenAddress_) external;
+    function setPriceProviderAddress(address providerAddress_) external;
+    function setReceiverAddress(address receiver_) external;
+    function setPeriodValidity(uint256 periodValidity_) external;
 }
